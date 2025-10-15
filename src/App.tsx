96465139a92545f0a64192fc12f061263c9df53f
Evgeny Simonov <jdsimonov2@gmail.com>
2025-10-15 14:30:52 +0300

    modified:   public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    src/App.test.tsx 	modified:   src/App.tsx 	new file:   src/logo.png

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Всем ку, я пишу курсач на React + TS.
         </p>

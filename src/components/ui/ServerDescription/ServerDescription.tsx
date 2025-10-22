de46dbf23d486dd9ab8dbca5282e26b3a5c445b8
Evgeny Simonov <jdsimonov2@gmail.com>
2025-10-22 13:30:33 +0300

    fix: more meaningful description

--- src/components/ui/ServerDescription/ServerDescription.tsx
+++ src/components/ui/ServerDescription/ServerDescription.tsx
@@ -5,7 +5,7 @@ const ServerDescription = () => {
   return (
     <div className="server-description-wrapper">
         <h1 className="server-desc-header">Железнодорожное<br/><span className="text-accent">кубическое путешествие</span></h1>
-        <p className="server-desc"><span className="text-accent">Project Novoakopovsk</span> — это Minecraft-сервер с упором на железные дороги, где вас ждут масштабные маршруты и атмосфера настоящего путешествия на поезде!</p>
+        <p className="server-desc"><span className="text-accent">Project Novoakopovsk</span> — это Minecraft-сервер, где вас ждут масштабные железнодорожные маршруты и атмосфера настоящего путешествия на поезде!</p>
     </div>
   );
 };
